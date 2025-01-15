911edca41e70e25b12835646f3990d725758fd95
AngelGuzmanNunez <Angelgabrielguzman28@gmail.com>
2025-01-14 20:25:57 -0400

    primer comentario

---
+++ 11_Working_with_Dates_and_Times/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Working_with_Dates_and_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime myValue = DateTime.Now;
+            //Displays both date and time.
+            //Console.WriteLine(myValue.ToString());
+
+            //Displays the date.
+            //Console.WriteLine(myValue.ToShortDateString());
+
+            //Displays the time.
+            //Console.WriteLine(myValue.ToShortTimeString());
+
+            //Displays the long version of the date.
+            //Console.WriteLine(myValue.ToLongDateString());
+
+            //Displays the long version of the time.
+            //Console.WriteLine(myValue.ToLongTimeString());
+
+
+            //Shows the date in 3 days time.
+            //Console.WriteLine(myValue.AddDays(3).ToLongDateString());
+
+            //Shows the time in 3 hours.
+            //Console.WriteLine(myValue.AddHours(3).ToLongTimeString());
+
+            //Shows the date in 3 days ago.
+            //Console.WriteLine(myValue.AddDays(-3).ToLongDateString());
+
+
+            //Shows the current month.
+            //Console.WriteLine(myValue.Month);
+
+            //Shows my birthday.
+            /*
+            DateTime myBirthday = new DateTime(1998, 8, 26);
+            Console.WriteLine(myBirthday.ToShortDateString());
+            */
+
+            DateTime myBirthday = new DateTime(1998, 8, 26);
+            TimeSpan myAge = DateTime.Now.Subtract(myBirthday);
+            Console.WriteLine(myAge.TotalDays);
+
+            Console.ReadLine();
+        }
+    }
+}
